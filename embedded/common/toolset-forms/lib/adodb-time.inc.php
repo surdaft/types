e4cc4fa1d8b96b84f0aa933806d3843f72aa62de
Jack Stupple <jack.stupple@modern-english.co.uk>
2019-03-27 14:32:54 +0000

    fix invalid break

--- embedded/common/toolset-forms/lib/adodb-time.inc.php
+++ embedded/common/toolset-forms/lib/adodb-time.inc.php
@@ -1009,7 +1009,6 @@ function adodb_tz_offset($gmt,$isphp5)
 		return sprintf('%s%02d%02d',($gmt<=0)?'+':'-',floor($zhrs),($zhrs-$hrs)*60); 
 	else
 		return sprintf('%s%02d%02d',($gmt<0)?'+':'-',floor($zhrs),($zhrs-$hrs)*60); 
-	break;
 }
 
 
